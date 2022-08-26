4d81f0ad51b50a4e4d7501bf4c3190f33eb6c7a8
¨Luis¨ <colucci.luis@gmail.com>
2022-08-25 21:14:45 -0300

    Tarefas

---
+++ tarefa-dia-2/pratica-1.js
@@ -0,0 +1,9 @@
+let nome = "Luis"
+let email = "colucci.luis@gmail.com"
+let profissao = "DevOps"
+let idade = 46
+let viagemDosSonhos = "Italy"
+
+let mensagem = "Olá, " + nome + "! Somos aqui da agência de turismo Viagem dos Sonhos. Estamos te escrevendo este email, pois acabamos de confirmar as compras das passagens aéreas para " + viagemDosSonhos + ". Confirma pra gente alguns dados? A sua idade é " + idade + ", você é " + profissao + " e o seu email é " + email + "? Ficamos no aguardo. Muito obrigado e boa viagem! :D"
+
+console.log(mensagem)
